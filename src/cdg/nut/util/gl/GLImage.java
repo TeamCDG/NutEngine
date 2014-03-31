6148daeeb9ae9c7200d2f083034e3e3a3975628a
Josh Theis <cdg.josh@googlemail.com>
2014-03-31 16:02:24 +0200

    basic 'thing' for image/colorquad display

---
+++ src/cdg/nut/util/gl/GLImage.java
@@ -0,0 +1,44 @@
+package cdg.nut.util.gl;
+
+import cdg.nut.interfaces.IDrawable;
+
+public class GLImage extends GLObject{
+
+	GLTexture image;
+	GLColor color;
+	
+	//TODO: get/set image
+	//TODO: get/set color
+	
+	//TODO: Javadoc
+	public GLImage(GLTexture image, float width, float height)
+	{
+		this(null, image, width, height, -1);
+	}
+	
+	//TODO: Javadoc
+	public GLImage(GLColor color, float width, float height)
+	{
+		this(color, null, width, height, -1);
+	}
+	
+	//TODO: Javadoc
+	public GLImage(float width, float height)
+	{
+		this(null, null, width, height, -1);
+	}
+	
+	//TODO: Javadoc
+	public GLImage(GLColor color, GLTexture image, float width, float height)
+	{
+		this(color, image, width, height, -1);
+	}
+	
+	//TODO: Javadoc
+	public GLImage(GLColor color, GLTexture image, float width, float height, int id)
+	{
+		//TODO: implement constructor
+	}
+
+
+}
