60df8109fe095faa40ad8a2034bed4e0e67d919b
Josh Theis <cdg.josh@googlemail.com>
2014-03-31 16:01:26 +0200

    added TODO

--- src/cdg/nut/util/gl/GLColor.java
+++ src/cdg/nut/util/gl/GLColor.java
@@ -9,22 +9,25 @@ public class GLColor {
 	private float b;
 	private float a;
 	
-	
+	//TODO: Javadoc
 	public GLColor(float[] color)
 	{
 		this(color[0], color[1], color[2], color[3]);
 	}
 	
+	//TODO: Javadoc
 	public GLColor(int id)
 	{
 		this(Utility.idToGlColor(id, false));
 	}
 	
+	//TODO: Javadoc
 	public GLColor(float r, float g, float b)
 	{
 		this(r,g,b,1.0f);
 	}
 	
+	//TODO: Javadoc
 	public GLColor(float r, float g, float b, float a)
 	{
 		this.r = r;
@@ -34,47 +37,55 @@ public class GLColor {
 		
 	}
 	
+	//TODO: Javadoc
 	public float[] toArray()
 	{
 		return new float[]{this.r,this.g,this.b,this.a};
 	}
 	
-	
+	//TODO: Javadoc
 	public float getR()
 	{
 		return this.r;
 	}
 	
+	//TODO: Javadoc
 	public void setR(float r)
 	{
 		this.r = r;
 	}
 	
+	//TODO: Javadoc
 	public float getG()
 	{
 		return this.g;
 	}
 	
+	//TODO: Javadoc
 	public void setG(float g)
 	{
 		this.g = g;
 	}
 	
+	//TODO: Javadoc
 	public float getB()
 	{
 		return this.b;
 	}
 	
+	//TODO: Javadoc
 	public void setB(float b)
 	{
 		this.b = b;
 	}
 	
+	//TODO: Javadoc
 	public float getA()
 	{
 		return this.a;
 	}
 	
+	//TODO: Javadoc
 	public void setA(float a)
 	{
 		this.a = a;
