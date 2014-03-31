5331f3e18300fb94af17199b5af726e3d7260d74
Josh Theis <cdg.josh@googlemail.com>
2014-03-31 16:02:53 +0200

    whatever

--- src/cdg/nut/gui/Component.java
+++ src/cdg/nut/gui/Component.java
@@ -1,5 +1,12 @@
 package cdg.nut.gui;
 
-public class Component {
+import cdg.nut.interfaces.ISelectable;
+import cdg.nut.util.gl.GLObject;
 
+public class Component extends GLObject{
+	
+	
+	public Component()
+	{
+	}
 }
