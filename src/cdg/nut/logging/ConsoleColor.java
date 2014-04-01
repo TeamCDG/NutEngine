0b544f12347422b763b52f6c9a68a909878ef04e
seishinryohosha <riegeranton@zoho.com>
2014-04-01 16:23:57 +0200

    This comments were way to dumb...

--- src/cdg/nut/logging/ConsoleColor.java
+++ src/cdg/nut/logging/ConsoleColor.java
@@ -2,9 +2,6 @@ package cdg.nut.logging;
 
 import cdg.nut.util.gl.GLColor;
 
-/** 
- * Enumeration of different colors for use in terminals
- */
 public enum ConsoleColor {
 	RESET("\u001B[0m", new GLColor(1.0f, 1.0f, 1.0f)),
 	BLACK("\u001B[30m", new GLColor(0.0f, 0.0f, 0.0f)),
@@ -15,22 +12,20 @@ public enum ConsoleColor {
 	PURPLE("\u001B[35m", new GLColor(1.0f, 0.0f, 1.0f)),
 	CYAN("\u001B[36m", new GLColor(0.0f, 1.0f, 1.0f)),
 	WHITE("\u001B[37m", new GLColor(1.0f, 1.0f, 1.0f));
-	
+
 	private String ansiColor;
 	private GLColor color;
-	
+
 	ConsoleColor(String ansi, GLColor color)
 	{
 		this.ansiColor = ansi;
 		this.color = color;
 	}
-	
-	/** Gets the ansi code for the given color */
+
 	public String getAnsiColor() {
 		return ansiColor;
 	}
-	
-	/** Gets the GLColor for the given color */
+
 	public GLColor getColor() {
 		return color;
 	}
