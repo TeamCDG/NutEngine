a625f00adb69fbdf9f7f4a6f85490450dfa293e7
Josh Theis <cdg.josh@googlemail.com>
2014-04-03 16:03:43 +0200

    support for known colors

--- src/cdg/nut/util/settings/Cmd.java
+++ src/cdg/nut/util/settings/Cmd.java
@@ -1,13 +1,67 @@
 package cdg.nut.util.settings;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import cdg.nut.logging.Logger;
 import cdg.nut.util.BitmapFont;
+import cdg.nut.util.Colors;
+import cdg.nut.util.Utility;
 import cdg.nut.util.gl.GLColor;
 
 //TODO: Javadoc
 public abstract class Cmd {
+	
+	public static void exec(String command)
+	{		
+		if(command.startsWith("#"))  //woops we have a comment here
+		{
+			return;
+		}
+		
+		
+		command = command.trim();	        	
+    	String key = command.split(" ")[0];
+    	List<String> parameter = new LinkedList<String>();
+    	command = command.replace(key, "").trim();
+    	
+    	while(command.length() != 0)
+    	{
+    		String param = "";
+    		if(command.startsWith("\""))
+    		{
+    			int secInd = 0;
+    			int c = 0;
+    			
+    			do
+                {
+                    c = 0;
+                    secInd = command.indexOf('"', secInd+1);
+                    while(secInd-(c+1)>= 0 && command.charAt(secInd-(c+1))=='\\')
+                    {
+                        c++;
+                    }
+                    
+                } while(c%2 != 0);
+    			
+    			param = command.substring(command.indexOf('"'), secInd+1);
+    		}
+			else if(command.startsWith("#")) //woops we have a comment here
+			{
+				break;
+			}
+    		else
+    		{
+    			param = command.split(" ")[0];
+    		}
+    		
+    		command = command.replace(param, "").trim();
+    		parameter.add(Utility.unescape(param.startsWith("\"") && param.endsWith("\"")?param.substring(1, param.length()-1):param));
+    		
+    	}
+    	
+    	Cmd.exec(key, parameter);
+	}
 	
 	public static void exec(String key, List<String> parameter)
 	{
@@ -56,7 +110,7 @@ public abstract class Cmd {
 						val = new GLColor(values[0], values[1], values[2]);
 					}
 				}
-				else
+				else if(parameter.size() == 4)
 				{
 					if(isInteger(parameter.get(0)))
 					{
@@ -85,6 +139,15 @@ public abstract class Cmd {
 							}
 						}
 						val = new GLColor(values[0], values[1], values[2], values[3]);
+					}
+				}
+				else
+				{
+					val = Colors.valueOf(parameter.get(0).toUpperCase()).getGlColor();
+					if(val == null)
+					{
+						Logger.error("Unable to change setting ("+key+"): illegel parameter "+0+" ("+parameter.get(0)+")", "CommandExecuter.exec");
+						return;
 					}
 				}
 			}
--- src/cdg/nut/util/settings/SettingsKeys.java
+++ src/cdg/nut/util/settings/SettingsKeys.java
@@ -12,15 +12,15 @@ public enum SettingsKeys {
 	WIN_ASPECT_RATIO("<float>", "aspect ration of window", float.class, SettingsAccess.READONLY),
 	WIN_RESOLUTION_CHANGED("", "event identifier", Object.class, SettingsAccess.PRIVATE),
 	
-	//---- REGION COMPONENTS -----
-	CMP_BACKGROUND_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]", "default background color of components", GLColor.class, SettingsAccess.PUBLIC),
-	CMP_BACKGROUND_H_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]", "default highlight background color of components", GLColor.class, SettingsAccess.PUBLIC),
-	CMP_FRAME_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]", "default frame color of components", GLColor.class, SettingsAccess.PUBLIC),
-	CMP_FRAME_H_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]", "default highlight frame color of components", GLColor.class, SettingsAccess.PUBLIC),
-	CMP_FONT("<string>", "default font of components", BitmapFont.class, SettingsAccess.PUBLIC),
-	CMP_FONT_SIZE("<int>/<float>", "default font of components", int.class, SettingsAccess.PUBLIC),
-	CMP_FONT_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]", "default text color of components", GLColor.class, SettingsAccess.PUBLIC),
-	CMP_FONT_H_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]", "default highlight text color of components", GLColor.class, SettingsAccess.PUBLIC),
+	//----- REGION GUI ------
+	GUI_CMP_BACKGROUND_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]/<string>", "default background color of components", GLColor.class, SettingsAccess.PUBLIC),
+	GUI_CMP_BACKGROUND_H_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]/<string>", "default highlight background color of components", GLColor.class, SettingsAccess.PUBLIC),
+	GUI_CMP_FRAME_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]/<string>", "default frame color of components", GLColor.class, SettingsAccess.PUBLIC),
+	GUI_CMP_FRAME_H_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]/<string>", "default highlight frame color of components", GLColor.class, SettingsAccess.PUBLIC),
+	GUI_CMP_FONT("<string>", "default font of components", BitmapFont.class, SettingsAccess.PUBLIC),
+	GUI_CMP_FONT_SIZE("<int>/<float>", "default font of components", int.class, SettingsAccess.PUBLIC),
+	GUI_CMP_FONT_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]/<string>", "default text color of components", GLColor.class, SettingsAccess.PUBLIC),
+	GUI_CMP_FONT_H_COLOR("<int> <int> <int> [int]/<float> <float> <float> [float]/<string>", "default highlight text color of components", GLColor.class, SettingsAccess.PUBLIC),
 	;
 	
 	private String parameterList;
