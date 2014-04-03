06d950e81258d794219f8d1c1868f07578b41a32
Josh Theis <cdg.josh@googlemail.com>
2014-04-04 01:13:25 +0200

    ColorBox and ImageBox components

--- src/cdg/nut/gui/components/ImageBox.java
+++ src/cdg/nut/gui/components/ImageBox.java
@@ -1,7 +1,25 @@
 package cdg.nut.gui.components;
 
 import cdg.nut.gui.Component;
+import cdg.nut.util.Colors;
+import cdg.nut.util.gl.GLImage;
+import cdg.nut.util.gl.GLTexture;
 
 public class ImageBox extends Component {
+
+	private GLImage image;
+	
+	public ImageBox(float x, float y, float width, float height, GLTexture image) {
+		super(x, y, width, height);
+		this.image = new GLImage(Colors.AQUA.getGlColor(), x, y, width, height);
+	}
+	
+	@Override
+	protected void drawChildren()
+	{
+		if(this.image.getShader() == null)
+			this.image.setShader(this.getShader());
+		//image.draw();
+	}
 
 }
