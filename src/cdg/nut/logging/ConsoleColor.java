be6018e5b57c77043d0bd3ded8fde005eef0b677
seishinryohosha <riegeranton@zoho.com>
2014-04-05 23:15:04 +0200

    Added Javadoc to ConsoleColor

--- src/cdg/nut/logging/ConsoleColor.java
+++ src/cdg/nut/logging/ConsoleColor.java
@@ -2,9 +2,9 @@ package cdg.nut.logging;
 
 import cdg.nut.util.gl.GLColor;
 
-/** 
- * Enumeration of different colors for use in terminals
- */
+/**
+ * This has 8 Colors.<br />
+ **/
 public enum ConsoleColor {
 	RESET("\u001B[0m", new GLColor(1.0f, 1.0f, 1.0f)),
 	BLACK("\u001B[30m", new GLColor(0.0f, 0.0f, 0.0f)),
@@ -15,22 +15,36 @@ public enum ConsoleColor {
 	PURPLE("\u001B[35m", new GLColor(1.0f, 0.0f, 1.0f)),
 	CYAN("\u001B[36m", new GLColor(0.0f, 1.0f, 1.0f)),
 	WHITE("\u001B[37m", new GLColor(1.0f, 1.0f, 1.0f));
-	
+
 	private String ansiColor;
 	private GLColor color;
-	
+
 	ConsoleColor(String ansi, GLColor color)
 	{
 		this.ansiColor = ansi;
 		this.color = color;
 	}
-	
-	/** Gets the ansi code for the given color */
+
+        /**
+         * Colors are:<br />
+         * Black, Blue, Cyan, Green, Purple, Red, White, Yellow<br />
+         *
+         * RESET equals WHITE
+         *
+         * @return ANSI-Colorcode as String
+         **/
 	public String getAnsiColor() {
 		return ansiColor;
 	}
-	
-	/** Gets the GLColor for the given color */
+
+        /**
+         * Colors are:<br />
+         * Black, Blue, Cyan, Green, Purple, Red, White, Yellow<br />
+         *
+         * RESET equals WHITE
+         *
+         * @return GLColor
+         **/
 	public GLColor getColor() {
 		return color;
 	}
