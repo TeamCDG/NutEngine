5ed3ecf1089401545d47b124abbc140402a7d815
seishinryohosha <riegeranton@zoho.com>
2014-04-06 00:35:22 +0200

    Reformated added Title

--- src/cdg/nut/logging/LogLevel.java
+++ src/cdg/nut/logging/LogLevel.java
@@ -1,35 +1,43 @@
 package cdg.nut.logging;
 
-/** 
- * Enumeration of different Logging/Debug Levels.
- */
+/**
+ * This has different Logging/Debug Levels.
+ **/
 public enum LogLevel {
 	SPAM(4, "SPAM"), DEBUG(3, "DEBUG"), INFO(2, "INFO"), ERROR(1, "ERROR"), NONE(0, "NUTS_ARE_AMAZING");
-	
+
 	private int level;
 	private String str;
-	
+
 	LogLevel(int lvl, String str)
 	{
 		this.level = lvl;
 		this.str = str;
 	}
-	
-	/** Gets the level.&nbsp;The higher, the more will be printed*/
+
+	/**
+         * Gets the level.<br />
+         *
+         * The higher, the more will be printed
+         **/
 	int getLevel()
 	{
 		return this.level;
 	}
-	
-	
-	/** Gets a String representation of the level */
-	@Override 
+
+
+	/**
+         * Gets a String representation of the level
+         **/
+	@Override
 	public String toString()
 	{
 		return this.str;
 	}
-	
-	/** Returns true if current level is greater than given level */
+
+	/**
+         * Returns true if current level is greater than given level
+         **/
 	public boolean isGreater(LogLevel lvl)
 	{
 		if(this.level > lvl.getLevel())
